7f4c77871e5a15e7b44b2bfeb1a82fff41a9b257
yasamanmansouri76 <mansouriyasaman82@gmail.com>
2023-05-10 03:01:45 +0430

    init project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <v-layout>
+    <v-app-bar color="primary" density="compact">
+      <template v-slot:prepend>
+        <RouterLink to="/" class="text-white text-h6 pa-2 mr-4">Home</RouterLink>
+        <RouterLink to="/about" class="text-white pa-2 text-h6">Todo List</RouterLink>
+      </template>
+    </v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <v-row dense>
+          <RouterView />
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-layout>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-card class="mx-auto pa-2" max-width="600">
+      <v-list>
+        <v-list-subheader class="text-h5">Todo Lists</v-list-subheader>
+        <v-list-item
+          v-for="(item, i) in todoLists"
+          :key="i"
+          :value="item"
+          active-color="primary"
+          rounded="xl"
+          lines="three"
+        >
+          <v-list-item-title class="align-center d-flex justify-space-between">
+            <span class="text-primary font-weight-medium">{{ item.text }}</span>
+            <span class="text-amber text-caption">12/2/2022</span>
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Require password for purchase or use password to restrict purchase
+          </v-list-item-subtitle>
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import { reactive } from 'vue'
+
+export default {
+  name: 'HomeView',
+  setup() {
+    const todoLists = reactive([
+      { text: 'Real-Time' },
+      { text: 'Audience' },
+      { text: 'Conversions' }
+    ])
+    return {
+      todoLists
+    }
+  }
+}
+</script>
